906a44c23605f9a853134b1b93652127ba469584
alex12392 <79908669+alex12392@users.noreply.github.com>
2021-03-03 13:10:14 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<!-- saved from url=(0030)https://buffaloslotsbonus.com/ -->
 <html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
